20e145a50ecf9e883d7fe6131252fab85734d847
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-10-20 14:52:13 -0500

    Created GET, and POST enpoints for Ticket entity

--- arji-backend/src/main/java/com/arji/arji_backend/controllers/TicketController.java
+++ arji-backend/src/main/java/com/arji/arji_backend/controllers/TicketController.java
@@ -1,7 +1,28 @@
 package com.arji.arji_backend.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.arji.arji_backend.models.Ticket;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TicketController {
+
+    @PostMapping("/auth/{projectId}/ticket")
+    public ResponseEntity<Ticket> createTicket(@PathVariable("projectId") Long projectId) {
+        return null;
+    }
+
+    @GetMapping("/auth/{projectId}/ticket")
+    public ResponseEntity<List<Ticket>> getAllTicketsForProject(@PathVariable("projectId") Long projectId) {
+        return null;
+    }
+
+    @GetMapping("/admin/ticket")
+    public ResponseEntity<List<Ticket>> getAllTickets() {
+        return null;
+    }
+
 }
