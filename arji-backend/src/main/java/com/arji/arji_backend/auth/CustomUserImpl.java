5c7aaa2673a5b80c96c06541ef928f9cd5beecc6
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-12-09 21:06:19 -0600

    Initialized UserDetails Implementation

---
+++ arji-backend/src/main/java/com/arji/arji_backend/auth/CustomUserImpl.java
@@ -0,0 +1,26 @@
+package com.arji.arji_backend.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class CustomUserImpl implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
