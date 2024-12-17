ab1dca994790f4c8401019344c3372e09b7375fd
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2024-12-17 17:06:10 -0600

    Bugfix: Fixed bug where user got redirected to /auth on page refresh

--- arji-frontend-react/src/store/index.js
+++ arji-frontend-react/src/store/index.js
@@ -1,26 +1,29 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialUserState = {
-    id: -1,
-    username: '',
-    roles: []
-}
+const loadState = () => {
+    const storedState = localStorage.getItem("authState");
+    return storedState ? JSON.parse(storedState) : { id: -1, username: '', roles: [] };
+};
+
+const initialUserState = loadState();
 
 const userSlice = createSlice({
     name: 'user',
     initialState: initialUserState,
     reducers: {
         login(state, action) {
-            console.log('here');
             state.id = action.payload.id;
             state.username = action.payload.username;
             state.roles = action.payload.roles;
-            console.log(state);
+
+            localStorage.setItem("authState", JSON.stringify(state));
         },
         logout(state, action) {
             state.id = -1;
             state.username = '';
             state.roles = [];
+
+            localStorage.removeItem("authState");
         }
     }
 });
